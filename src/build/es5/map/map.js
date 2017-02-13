a0746f8bb2d60c79c442f69979551c01844ef411
FDD <smileFDD@gmail.com>
2017-02-13 15:18:47 +0800

    ??

--- src/build/es5/map/map.js
+++ src/build/es5/map/map.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -534,7 +538,10 @@ var HDMap = function () {
           }
         } else if (params['featureType']) {
           imgSrc = config.markConfig.getMarkConfigByType(params['featureType']).imgURL;
-          imgSrcHover = config.markConfig.getMarkConfigByType(params['featureType']).imgURL;
+          imgSrcHover = config.markConfig.getMarkConfigByType(params['featureType']).hover;
+          if (!imgSrcHover) {
+            imgSrcHover = imgSrc;
+          }
         } else {
           imgSrc = config.markConfig.getDefaultMrakConfig().imgURL;
           imgSrcHover = config.markConfig.getDefaultMrakConfig().imgURL;
@@ -1298,3 +1305,5 @@ var HDMap = function () {
 
   return HDMap;
 }();
+
+exports.default = HDMap;
